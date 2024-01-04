c4f493cc9b465f3a94836619b6d964243f6f88a6
Luis Carlos Jarero Martinez <ljarero@ia.com.mx>
2024-01-04 11:40:26 -0600

    feature-loader

--- components/Templates/Templates.tsx
+++ components/Templates/Templates.tsx
@@ -3,7 +3,7 @@ import { Websites, WebsitesLayout } from "../../tina/__generated__/types";
 import TraditionalTemplates from "./TraditionalTemplates/TraditionalTemplates";
 import { useContext, useEffect } from "react";
 import { SharedStateContext } from "../../context/layoutContext";
-
+import { motion } from "framer-motion";
 const TemplateLayout = (props: Omit<Websites, "id" | "_sys" | "_values">) => {
   const setData = useContext(SharedStateContext);
   const handleData = () => {
@@ -17,13 +17,15 @@ const TemplateLayout = (props: Omit<Websites, "id" | "_sys" | "_values">) => {
       {props.layout
         ? props.layout.map(function (block, i) {
             return (
-              <div
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: [0.8, 1] }}
                 key={i}
                 className="flex flex-col relative w-screen h-screen max-h-screen"
                 data-tina-field={tinaField(block)}
               >
                 <Template {...block} />
-              </div>
+              </motion.div>
             );
           })
         : null}
--- pages/websites/[filename].tsx
+++ pages/websites/[filename].tsx
@@ -6,11 +6,11 @@ import { WebsitesQuery } from "../../tina/__generated__/types";
 import TemplateLayout from "../../components/Templates/Templates";
 import { SharedStateProvider } from "../../context/layoutContext";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 const ServiceList = lazy(
   () => import("../../components/ServicesList/ServiceList")
 );
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 
 export default function WebPage(
   props: InferGetStaticPropsType<typeof getStaticProps>
@@ -34,32 +34,55 @@ export default function WebPage(
     };
     return SEOData;
   };
+
+  const [loaderPage, setLoaderPage] = useState(
+    props.data.websites.loader.isLoader
+  );
+  const handleActive = () => {
+    setTimeout(() => {
+      setLoaderPage(false);
+    }, 1000);
+  };
+  useEffect(() => {
+    handleActive();
+  }, []);
   return (
     <SharedStateProvider>
       <Suspense fallback={() => <div> Loading bb </div>}>
         <SEOLayout {...SEOAdapter(data)}>
-          {props.data.websites.loader.isLoader ? (
-            <motion.div
-              className="w-screen h-screen flex items-center justify-center absolute"
-              style={{
-                backgroundColor: props.data.websites.loader.backgroundColor,
-              }}
-            >
-              <motion.picture>
-                <Image
-                  src={props.data.websites.loader.loaderImage}
-                  width={60}
-                  height={60}
-                  alt=""
-                />
-              </motion.picture>
-            </motion.div>
-          ) : (
-            <>
-              <TemplateLayout {...data.websites} />
-              <ServiceList {...data.websites} />
-            </>
-          )}
+          <AnimatePresence mode="sync">
+            {loaderPage ? (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: [1] }}
+                transition={{ type: "spring" }}
+                exit={{ opacity: 0 }}
+                key="loader"
+                className="w-screen h-screen flex items-center justify-center absolute"
+                style={{
+                  backgroundColor: props.data.websites.loader.backgroundColor,
+                }}
+              >
+                <motion.picture
+                  initial={{ opacity: 1 }}
+                  animate={{ opacity: [0, 0, 0.5, 0.8, 1], scale: [1, 1.3, 1] }}
+                  exit={{ opacity: 0 }}
+                >
+                  <Image
+                    src={props.data.websites.loader.loaderImage}
+                    width={60}
+                    height={60}
+                    alt=""
+                  />
+                </motion.picture>
+              </motion.div>
+            ) : (
+              <>
+                <TemplateLayout {...data.websites} />
+                <ServiceList {...data.websites} />
+              </>
+            )}
+          </AnimatePresence>
         </SEOLayout>
       </Suspense>
     </SharedStateProvider>
